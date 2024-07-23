31a30fc14d14bb42105e340aef89dd57132cc908
SaiRuthwik22 <160124679+SaiRuthwik22@users.noreply.github.com>
2024-07-23 09:59:53 +0530

    Update Selection.js

--- src/components/Selection.js
+++ src/components/Selection.js
@@ -1,1 +1,19 @@
+import React from 'react';
 
+const Selection = ({ applyColor, nextBackground }) => {
+  const handleClick = () => {
+    applyColor(nextBackground);
+  };
+
+  return (
+    <div
+      className="selection"
+      style={{ background: nextBackground.background }}
+      onClick={handleClick}
+    >
+      Select me
+    </div>
+  );
+};
+
+export default Selection;
