b746e829a73168b0787ac3fecf2587b9079e0152
SaiRuthwik22 <160124679+SaiRuthwik22@users.noreply.github.com>
2024-07-23 16:48:51 +0530

    Update ColourSelector.js

--- src/components/ColourSelector.js
+++ src/components/ColourSelector.js
@@ -4,7 +4,7 @@ const ColourSelector = (props) => {
   const { config, selectNextBackground } = props
   const { background } = config;
   return (
-    <button className={config.classname} data-testid={config.label} id={config.label} onClick={() => selectNextBackground({background: background})}>
+    <button className={config.classname} data-testid={`${config.label}`} onClick={() => selectNextBackground({background: background})}>
       {config.label}
     </button>
   )
